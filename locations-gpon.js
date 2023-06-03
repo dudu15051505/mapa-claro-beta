d62f4e28ec886927c63ad211e976f45c6604259f
GitHub Action <action@github.com>
2023-06-03 22:33:05 +0000

    updated logs

---
+++ locations-gpon.js
@@ -0,0 +1,1 @@
+{ name: 'Dois Irmãos, RS <br> GPON', color: 'green', latitude: '-29.58356', longitude: '-51.089776' },
---
+++ locations-hfc.js
@@ -0,0 +1,1 @@
+{ name: 'Mogi das Cruzes, SP <br> HFC', color: 'red', latitude: '-23.52082', longitude: '-46.18541' },
