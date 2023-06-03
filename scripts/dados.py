328229de894486d89df22a4168405fe871bc4124
dudu15051505 <gustavoramos1234@gmail.com>
2023-06-03 20:00:53 -0300

    Update dados.py

--- scripts/dados.py
+++ scripts/dados.py
@@ -75,7 +75,7 @@ for i in range(len(cities)):
         # Aguardar 3 segundos antes da próxima solicitação
         time.sleep(3)
 
-f = open("/home/runner/work/claro-mapa-privado/claro-mapa-privado/scripts/dados/data-update.js", "w")
+f = open("/home/runner/work/claro-mapa-privado/claro-mapa-privado/scripts/js/data-update.js", "w")
 from datetime import date
 today = date.today()
 # dd/mm/YY
