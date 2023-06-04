9e95f081f2d614aadc7534894ede1aedf235cca5
dudu15051505 <gustavoramos1234@gmail.com>
2023-06-04 10:06:54 -0300

    update scripts

--- scripts/js - Copia/locations-sobrepo.js
+++
@@ -1,6 +0,0 @@
-var locations_sobrepo = [
-/* CIDADES ADICIONADAS MANUALMENTE, POR ERRO NO CADASTRO DE CONSULTA API DA CLARO */
-{ name: 'Mogi das Cruzes, SP <br> Sobreposição HFC e GPON', color: 'yellow', latitude: '-23.52082', longitude: '-46.18541' },
-
-/* CIDADES CONSULTADAS AUTOMATICAMENTE PELO SCRIPT */
-];
