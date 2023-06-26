9cbbdc0e0a9d2f3131a626023b1b7ace27badba8
dudu15051505 <gustavoramos1234@gmail.com>
2023-06-25 21:00:49 -0300

    correção

--- scripts/dados.py
+++ scripts/dados.py
@@ -95,4 +95,4 @@ f.close()
 today = date.today()
 d2 = today.strftime("%Y-%m-%d")
 with open(diretorio_trabalho + 'js/data-lista.txt', 'a') as my_file:
-    my_file.write(d2 + '\n')
+    my_file.write('\n' + d2)
