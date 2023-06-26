48e574be4bdcc66e541afc2b04bf34bc69ba5f1d
dudu15051505 <gustavoramos1234@gmail.com>
2023-06-25 21:22:27 -0300

    test script finalizado

--- docs/js/old/2023-06-25/locations-sobrepo.js
+++
@@ -1,9 +0,0 @@
-var locations_sobrepo = [
-/* CIDADES ADICIONADAS MANUALMENTE, POR ERRO NO CADASTRO DE CONSULTA API DA CLARO */
-{ name: 'Mogi das Cruzes, SP <br> Sobreposição HFC e GPON', color: 'yellow', latitude: '-23.52082', longitude: '-46.18541' },
-{ name: 'Palmas, TO <br> Sobreposição HFC e GPON', color: 'yellow', latitude: '-10.239973', longitude: '-48.355751' },
-
-/* CIDADES CONSULTADAS AUTOMATICAMENTE PELO SCRIPT */
-{ name: 'Rio de Janeiro, RJ <br> Sobreposição HFC e GPON', color: 'yellow', latitude: '-22.912897', longitude: '-43.200295' },
-{ name: 'São Paulo, SP <br> Sobreposição HFC e GPON', color: 'yellow', latitude: '-23.532905', longitude: '-46.63952' }
-];
