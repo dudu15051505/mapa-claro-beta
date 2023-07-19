bf66ee098bcdb22a117a651eaea0e4cd594faaa6
dudu15051505 <gustavoramos1234@gmail.com>
2023-07-19 20:12:07 -0300

    dados apenas cidades relacionadas pdf

--- scripts/dados.py
+++ scripts/dados.py
@@ -96,4 +96,4 @@ f.close()
 today = date.today()
 d2 = today.strftime("%Y-%m-%d")
 with open(diretorio_trabalho + 'js/data-lista.txt', 'a') as my_file:
-    my_file.write('\n' + d2)
+    my_file.write('\n "' + d2 + '","' + d2 + '","consulta automática","#",""')
